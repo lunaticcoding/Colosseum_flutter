daf5d9a59230cc1f7abdc1bdf598de1c4b9db937
Marco Papula <ga84div@mytum.de>
2019-09-11 12:16:11 +0200

    Techfest Status Quo

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:colosseum/trex/logic/Game.dart';
+import 'package:colosseum/trex/trexgame.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_livestream_ml_vision/firebase_livestream_ml_vision.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui' as ui;
+
+Future<void> main() async {
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeRight]);
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  Flame.audio.disableLog();
+  List<ui.Image> image = await Flame.images.loadAll(["sprite.png"]);
+  TRexGame tRexGame = TRexGame(spriteImage: image[0]);
+
+  runApp(MyApp(tRexGame: tRexGame,));
+
+//  Flame.util.
+//  Flame.util.addGestureRecognizer(new TapGestureRecognizer()
+//    ..onTapDown = (TapDownDetails evt) => tRexGame.onTap());
+}
+
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key, @required this.tRexGame}) : super(key: key);
+  final TRexGame tRexGame;
+
+  @override
+  _MyAppState createState() => _MyAppState(tRexGame: tRexGame);
+}
+
+class _MyAppState extends State<MyApp> {
+  final TRexGame tRexGame;
+  FirebaseVision _vision;
+  double range;
+
+  _MyAppState({this.tRexGame});
+
+  @override
+  void initState() {
+    super.initState();
+    range = 0.0;
+    _initializeCamera();
+  }
+
+  void _initializeCamera() async {
+    List<FirebaseCameraDescription> cameras = await camerasAvailable();
+    _vision = FirebaseVision(cameras[1], ResolutionSetting.high);
+    _vision.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      _vision.addFaceDetector().then((faces) {
+        faces.asBroadcastStream().listen((faces_data) {
+          if (faces_data.length != 0) {
+            setState(() {
+              double dist_in_pix = faces_data[0].boundingBox.right -
+                  faces_data[0].boundingBox.left;
+              dist_in_pix = (dist_in_pix > 600) ? 600 : dist_in_pix;
+              dist_in_pix = (dist_in_pix < 250) ? 250 : dist_in_pix;
+              range = 1.0 - (dist_in_pix - 250) / (600 - 250);
+              tRexGame.onValueUpdate(range);
+            });
+          }
+        });
+      });
+      setState(() {});
+    });
+  }
+
+  void dispose() {
+    _vision.dispose().then((_) {
+      // close all detectors
+    });
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotatedBox(
+      quarterTurns: 2,
+      child: GameWrapper(tRexGame),
+    );
+  }
+}
---
+++ lib/trex/logic/Game.dart
@@ -0,0 +1,104 @@
+import 'dart:ui';
+
+import 'package:colosseum/trex/logic/t_rex/config.dart';
+import 'package:colosseum/trex/logic/t_rex/t_rex.dart';
+import 'package:flame/game.dart';
+
+import 'collision/collision_utils.dart';
+import 'game_config.dart';
+import 'game_over/game_over.dart';
+import 'horizon/horizon.dart';
+
+enum TRexGameStatus { playing, waiting, gameOver }
+
+class TRexGame extends BaseGame {
+  bool isDown = false;
+  TRex tRex;
+  Horizon horizon;
+  GameOverPanel gameOverPanel;
+  TRexGameStatus status = TRexGameStatus.waiting;
+
+  double currentSpeed = GameConfig.speed;
+  double timePlaying = 0.0;
+
+  TRexGame({Image spriteImage}) {
+    tRex = new TRex(spriteImage);
+    horizon = new Horizon(spriteImage);
+    gameOverPanel = new GameOverPanel(spriteImage);
+
+    this..add(horizon)..add(tRex)..add(gameOverPanel);
+  }
+
+  void onValueUpdate(double value) {
+    if(value < 0.5) {
+      isDown = true;
+    } else {
+      if (gameOver) {
+        restart();
+        return;
+      }
+      if (isDown) {
+        tRex.startJump(this.currentSpeed);
+        isDown = false;
+      }
+    }
+  }
+
+  @override
+  void update(double t) {
+    tRex.update(t);
+    horizon.updateWithSpeed(0.0, this.currentSpeed);
+
+    if (gameOver) return;
+
+    if (tRex.playingIntro && tRex.x >= TRexConfig.startXPos) {
+      startGame();
+    } else if (tRex.playingIntro) {
+      horizon.updateWithSpeed(0.0, this.currentSpeed);
+    }
+
+    if (this.playing) {
+      timePlaying += t;
+      horizon.updateWithSpeed(t, this.currentSpeed);
+
+      var obstacles = horizon.horizonLine.obstacleManager.components;
+      bool collision =
+          obstacles.length > 0 && checkForCollision(obstacles.first, tRex);
+      if (!collision) {
+        if (this.currentSpeed < GameConfig.maxSpeed) {
+          this.currentSpeed += GameConfig.acceleration;
+        }
+      } else {
+        doGameOver();
+      }
+    }
+  }
+
+  void startGame() {
+    tRex.status = TRexStatus.running;
+    status = TRexGameStatus.playing;
+    tRex.hasPlayedIntro = true;
+  }
+
+  bool get playing => status == TRexGameStatus.playing;
+  bool get gameOver => status == TRexGameStatus.gameOver;
+
+  void doGameOver() {
+    this.gameOverPanel.visible = true;
+    stop();
+    tRex.status = TRexStatus.crashed;
+  }
+
+  void stop() {
+    this.status = TRexGameStatus.gameOver;
+  }
+
+  void restart() {
+    status = TRexGameStatus.playing;
+    tRex.reset();
+    horizon.reset();
+    currentSpeed = GameConfig.speed;
+    gameOverPanel.visible = false;
+    timePlaying = 0.0;
+  }
+}
